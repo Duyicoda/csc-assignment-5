23d99e82416d9e78a5cd2555e77476681b40d821
precious <preciousduyilemi00@gmail.com>
2025-01-31 08:38:05 -0800

    done

---
+++ Ekeh Delight/Ekeh Delight/cos assignment/form.js
@@ -0,0 +1,9 @@
+
+function hi(){
+let email = document.getElementById("Email").value;
+let Password = document.getElementById("Password").value;
+let htwo = document.getElementById("h2");
+  alert(email)
+
+
+  }
